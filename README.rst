02afdb0c4a4b6f4202feb72801d622ceb071437e
David Sanders <dsanders@fusionbox.com>
2013-03-04 12:58:58 -0700

    First commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+======
+schism
+======
+
+Setup and configure webfaction site with xmlrpc.
+
+License
+=======
+
+Please take a moment to review the license governing this software package as
+specified in the ``LICENSE`` file.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='schism',
+      version='0.1',
+      description='Setup and configure webfaction sites with xmlrpc.',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords='webfaction xmlrpc configuration setup',
+      url='https://github.com/davesque/schism',
+      author='David Sanders',
+      author_email='davesque@gmail.com',
+      license='MIT',
+      packages=['schism'],
+      scripts=['bin/schism'],
+      zip_safe=False)
