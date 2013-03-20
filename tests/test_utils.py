c66faa93d35acd894ca504a0c916fbbf5a4239e7
David Sanders <dsanders@fusionbox.com>
2013-03-20 00:23:27 -0600

    Update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,20 +6,21 @@ from schism.utils import log
 
 class UtilsTestCase(unittest.TestCase):
     def setUp(self):
-        self.stderr = MagicMock()
-        self.log = patch('sys.stderr', self.stderr)(log)
+        self.patcher = patch('sys.stderr')
+        self.mock_stderr = self.patcher.start()
+        self.addCleanup(self.patcher.stop)
 
     def test_log_without_function_args_logs_exact_message(self):
-        self.log('Starborks')
-        self.stderr.write.assert_called_with('Starborks')
+        log('Starborks')
+        self.mock_stderr.write.assert_called_with('Starborks')
 
     def test_log_with_function_args_logs_extra_messages_and_runs_function(self):
         test_func = MagicMock()
 
-        self.log('Running test function', test_func, 1, 'arst', foo='bar')
+        log('Running test function', test_func, 1, 'arst', foo='bar')
 
         self.assertEqual(
-            self.stderr.write.mock_calls,
+            self.mock_stderr.write.mock_calls,
             [call('Running test function'), call('...'), call('done\n')],
         )
         test_func.assert_called_with(1, 'arst', foo='bar')
